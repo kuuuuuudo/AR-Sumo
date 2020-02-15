87b362130248c7b3a4746310acd3c1d5d3b3979e
kudone <ku.kn8821@gmail.com>
2020-02-15 21:51:31 +0900

    rb

---
+++ Assets/My/Scripts/BattleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleButton : MonoBehaviour
+{
+
+    public GameObject player,enemy;
+    Rigidbody playerRb,enemyRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = player.transform.GetComponent<Rigidbody>();
+        enemyRb = enemy.transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        playerRb.isKinematic = false;
+        enemyRb.isKinematic = false;
+    }
+
+}
---
+++ Assets/My/Scripts/TapController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapController : MonoBehaviour
+{
+    public GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("タップされました");
+            Vector3 tmp = target.transform.position;
+            target.transform.position = new Vector3(tmp.x, tmp.y, tmp.z + 0.1f);
+            Debug.Log(tmp);
+        }
+
+        if (OnTouchDown())
+        {
+            Debug.Log("タップされました");
+            Instantiate(target, new Vector3(1.0f, 2.0f, 0.0f), Quaternion.identity);
+        }
+    }
+
+    //スマホ向け そのオブジェクトがタッチされていたらtrue（マルチタップ対応）
+    bool OnTouchDown()
+    {
+        // タッチされているとき
+        if (0 < Input.touchCount)
+        {
+            // タッチされている指の数だけ処理
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                // タッチ情報をコピー
+                Touch t = Input.GetTouch(i);
+                // タッチしたときかどうか
+                if (t.phase == TouchPhase.Began)
+                {
+                    //タッチした位置からRayを飛ばす
+                    Ray ray = Camera.main.ScreenPointToRay(t.position);
+                    RaycastHit hit = new RaycastHit();
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        //Rayを飛ばしてあたったオブジェクトが自分自身だったら
+                        if (hit.collider.gameObject == this.gameObject)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false; //タッチされてなかったらfalse
+    }
+}
